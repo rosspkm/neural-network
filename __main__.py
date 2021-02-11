b85bd98be5ceaa45badb57bd43e87fcd032ee447
rosspkm <rosspkm@gmail.com>
2021-02-10 21:39:40 -0500

    made updates

--- __main__.py
+++ __main__.py
@@ -1,1 +1,36 @@
 # neural network start
+import numpy as np
+import pandas as pd
+
+class data:
+
+    def __init__(self, data:Dataframe, labels:Dataframe):
+        self.data = data
+        self.labels = labels
+    
+    def get_data(self):
+        return self.data
+    
+    def get_labels(self):
+        return self.labels
+
+
+class NeuralNetwor:
+
+    def __init__(self, x:list):
+        self.input = x
+        
+
+data = "./data/train.csv"
+
+df = pd.read_csv(data)
+labels = pd.DataFrame(df.loc[: , 'label'])
+df.drop(['label'], axis=1, inplace=True)
+
+
+MNIST = data(data=df, labels=labels)
+
+# print(len(df.index))
+# for i in range(0, len(df.index)):
+#     print(df.loc[i].values)
+
